9ce4bede6e6f9f3bb915fda204d2a14fce817980
max-student <max-student@maxtrain.com>
2020-02-19 16:39:16 -0500

    Add project files.

---
+++ SQLLibrary/BcConnection.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace SQLLibrary {
+    
+    public class BcConnection  {
+
+        public SqlConnection Connection { get; set; } //SQL connection example
+
+        //Connection string   first parameter @"server=localhost\sqlexpress; second parameter database=EdDb; third parameter trusted_connection=true;";
+
+        public void Connect(string server, string database, string auth) { //This is how to connect make a connection to sql
+            var connStr = ($"Server ={server}; database = {database};{auth}");
+            Connection = new SqlConnection(connStr);
+            Connection.Open(); //Trap catch or no conncetion can blow up the program.
+            
+            if(Connection.State != System.Data.ConnectionState.Open) { //How to know if your conncetion is good
+                Console.WriteLine("Could not open the connection -- check connnection string");
+                Connection = null;
+                return;
+            }
+            Console.WriteLine("Connection opened");
+        }
+        public void Disconnect() { //Disconnect the sql connection and return it
+            if(Connection == null) {
+                return;
+            }
+            if (Connection.State == System.Data.ConnectionState.Open) {
+                Connection.Close();
+                Connection.Dispose();
+                Connection = null;
+            }
+        }
+
+    }
+}
